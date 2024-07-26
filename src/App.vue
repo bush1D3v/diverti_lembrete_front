965f2cbeb043bdc1d482cd3ade301e221738ad68
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:06:48 -0300

    update: Add popups components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Sonner from "./components/ui/sonner/Sonner.vue";
 import Header from "./components/Header.vue";
 import SideBar from "./components/SideBar.vue";
+import Toaster from "./components/ui/toast/Toaster.vue";
 </script>
 
 <template>
@@ -9,5 +11,7 @@ import SideBar from "./components/SideBar.vue";
     <main class="min-h-[95dvh] bg-darkAlt relative flex items-center gap-4">
         <SideBar />
         <RouterView />
+        <Toaster />
+        <Sonner />
     </main>
 </template>
