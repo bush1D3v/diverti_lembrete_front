2f2382da7b5412c49473b0022fd36d3b79bbfb5d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:13:47 -0300

    update: Update graphs view page

--- src/views/Graphs.vue
+++ src/views/Graphs.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CheckGraph from "@/components/CheckGraph.vue";
+import EmotionGraph from "@/components/EmotionGraph.vue";
+</script>
 
 <template>
-    <h1 class="text-center text-4xl">GRÁFICOS</h1>
+    <div class="flex justify-center items-center w-full">
+        <ul class="flex justify-around items-center w-10/12 min-h-[95dvh]">
+            <li class="flex flex-col text-center w-6/12 relative">
+                <CheckGraph />
+            </li>
+            <li class="flex flex-col text-center w-6/12 relative">
+                <EmotionGraph />
+            </li>
+        </ul>
+    </div>
 </template>
