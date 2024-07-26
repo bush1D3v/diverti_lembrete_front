47443ddb82386819b8b99d0f2a2c28dec2bc5dc9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:15:51 -0300

    feat: Create whatsapp icon component

---
+++ src/components/WhatsAppIcon.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { whatsappSender } from "@/api";
+import AlertDialog from "./AlertDialog.vue";
+
+const props = defineProps<{
+    text: string;
+    textClass: string;
+}>();
+</script>
+
+<template>
+    <AlertDialog
+        :title="'Deseja mesmo fazer isso?'"
+        :description="'Fazendo isso, você enviará o lembrete para o seu WhatsApp.'"
+        :function="whatsappSender"
+        :id="
+            props.text +
+            '\n\n' +
+            'Data: ' +
+            new Date().toLocaleString('pt-BR', {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric'
+            })
+        "
+        :tooltip="'Notificar WhatsApp'"
+    >
+        <template #trigger>
+            <i
+                class="fi fi-brands-whatsapp text-light hover:text-green-500 cursor-pointer ease-in-out duration-75"
+                :class="props.textClass"
+            />
+        </template>
+    </AlertDialog>
+</template>
