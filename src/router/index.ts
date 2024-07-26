02b117932e9f45adcf5f20ac3b9fefd640400d78
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:21:14 -0300

    update: Update vue router routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,20 @@ export default createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: "/",
-            name: "Home",
-            component: () => import("@/views/Home.vue")
+            path: '/',
+            redirect: '/home?page=1'
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: () => import("@/views/Home.vue"),
+            beforeEnter: (to, from, next) => {
+                if (!to.query.page) {
+                    next({ path: '/home', query: { page: '1' } });
+                } else {
+                    next();
+                }
+            }
         },
         {
             path: "/settings",
@@ -34,14 +45,27 @@ export default createRouter({
             component: () => import("@/views/ReminderDetail.vue")
         },
         {
-            path: "/reminders/:emotion",
+            path: "/reminders/:emotion/",
             name: "List Emotion Reminders",
-            component: () => import("@/views/RemindersEmotion.vue")
+            component: () => import("@/views/RemindersEmotion.vue"),
+            beforeEnter: (to, from, next) => {
+                const emotion = to.params.emotion;
+                if (!to.query.page) {
+                    next({ path: `/reminders/${emotion}`, query: { page: '1' } });
+                } else {
+                    next();
+                }
+            },
+            props: route => ({ key: route.params.emotion })
+        },
+        {
+            path: "/not-found/:emotion?",
+            name: "Not Found",
+            component: () => import("@/views/NotFound.vue")
         },
         {
             path: "/:pathMatch(.*)*",
-            name: "Not Found",
-            component: () => import("@/views/NotFound.vue")
+            redirect: { name: "Not Found" }
         }
     ]
 });
