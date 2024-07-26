13131d076b2889d8db791a61e3881e26e3d1c9e9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:15:42 -0300

    feat: Create trash icon component

---
+++ src/components/TrashIcon.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { reminderDelete } from "@/api";
+import AlertDialog from "./AlertDialog.vue";
+import type { RouteLocationNormalizedLoadedGeneric, Router } from "vue-router";
+
+const props = defineProps<{
+    id: string;
+    total: number;
+    textClass: string;
+    router: Router;
+    route: RouteLocationNormalizedLoadedGeneric;
+}>();
+</script>
+
+<template>
+    <AlertDialog
+        :title="'Deseja mesmo fazer isso?'"
+        :description="'Fazendo isso, você estará excluindo seu lembrete para sempre. Isso não pode ser desfeito.'"
+        :function="reminderDelete"
+        :router="props.router"
+        :route="props.route"
+        :total="props.total"
+        :id="props.id"
+        :tooltip="'Excluir lembrete'"
+    >
+        <template #trigger>
+            <span
+                class="material-icons text-light hover:text-red-500 cursor-pointer ease-in-out duration-75"
+                :class="props.textClass"
+                >delete_forever</span
+            >
+        </template>
+    </AlertDialog>
+</template>
