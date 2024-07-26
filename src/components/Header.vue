33d38f3480c80a31c691d6a9b3fe4cc3b0c415f0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:16:03 -0300

    update: Update header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ButtonTheme from "./ButtonTheme.vue";
 import Clock from "./Clock.vue";
 import SearchBar from "./SearchBar.vue";
 </script>
@@ -7,5 +8,6 @@ import SearchBar from "./SearchBar.vue";
     <header class="max-h-[5dvh] bg-primary flex px- py-1 justify-center">
         <Clock />
         <SearchBar />
+        <ButtonTheme />
     </header>
 </template>
