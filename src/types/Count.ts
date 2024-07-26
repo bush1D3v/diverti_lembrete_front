752f44660456b2299f0c2ce4d5ef6fb824c9518c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:09:47 -0300

    feat: Create Count interface

---
+++ src/types/Count.ts
@@ -0,0 +1,14 @@
+export interface Count {
+    total: number;
+    checks: number;
+    unchecks: number;
+    happiness: number;
+    sad: number;
+    angry: number;
+    anxiety: number;
+    envy: number;
+    shame: number;
+    fear: number;
+    disgust: number;
+    boredom: number;
+}
