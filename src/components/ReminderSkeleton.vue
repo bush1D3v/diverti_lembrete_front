3f5384c62ad9d12f4280ab069d454a7ed23f2629
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:15:29 -0300

    feat: Create reminder skeleton component

---
+++ src/components/ReminderSkeleton.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { Skeleton } from "./ui/skeleton";
+</script>
+
+<template>
+    <div class="grid grid-cols-5 justify-center grid-rows-2 gap-20">
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+        <Skeleton class="w-56 h-56 rounded-md" />
+    </div>
+</template>
