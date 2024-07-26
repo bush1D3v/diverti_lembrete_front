4977130e6214d3a6b4e2403c3940564872627943
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:16:46 -0300

    feat: Create shadcn alert dialog component

--- src/components/ui/alert-dialog/AlertDialogAction.vue
+++ src/components/ui/alert-dialog/AlertDialogAction.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import { AlertDialogAction, type AlertDialogActionProps } from 'radix-vue'
-import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
+import { type HTMLAttributes, computed } from "vue";
+import { AlertDialogAction, type AlertDialogActionProps } from "radix-vue";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
-const props = defineProps<AlertDialogActionProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<AlertDialogActionProps & { class?: HTMLAttributes["class"] }>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+    const { class: _, ...delegated } = props;
 
-  return delegated
-})
+    return delegated;
+});
 </script>
 
 <template>
-  <AlertDialogAction v-bind="delegatedProps" :class="cn(buttonVariants(), props.class)">
-    <slot />
-  </AlertDialogAction>
+    <AlertDialogAction v-bind="delegatedProps" :class="cn(buttonVariants(), props.class)">
+        <slot />
+    </AlertDialogAction>
 </template>
--- src/components/ui/alert-dialog/AlertDialogDescription.vue
+++ src/components/ui/alert-dialog/AlertDialogDescription.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import {
-  AlertDialogDescription,
-  type AlertDialogDescriptionProps,
-} from 'radix-vue'
-import { cn } from '@/lib/utils'
+import { type HTMLAttributes, computed } from "vue";
+import { AlertDialogDescription, type AlertDialogDescriptionProps } from "radix-vue";
+import { cn } from "@/lib/utils";
 
-const props = defineProps<AlertDialogDescriptionProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<AlertDialogDescriptionProps & { class?: HTMLAttributes["class"] }>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+    const { class: _, ...delegated } = props;
 
-  return delegated
-})
+    return delegated;
+});
 </script>
 
 <template>
-  <AlertDialogDescription
-    v-bind="delegatedProps"
-    :class="cn('text-sm text-muted-foreground', props.class)"
-  >
-    <slot />
-  </AlertDialogDescription>
+    <AlertDialogDescription v-bind="delegatedProps" :class="cn('text-sm text-accent', props.class)">
+        <slot />
+    </AlertDialogDescription>
 </template>
