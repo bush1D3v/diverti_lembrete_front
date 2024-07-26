8666fcb51933be90d5c6e2150d4453443d87e558
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:16:11 -0300

    update: Update search bar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from "vue";
 import { useMagicKeys } from "@vueuse/core";
-import { PlusIcon, CountdownTimerIcon } from "@radix-icons/vue";
+import { PlusIcon } from "@radix-icons/vue";
 import { RoutesEnum } from "@/enum/RoutesEnum";
 import {
     Command,
@@ -18,13 +18,19 @@ const inputValue = ref("");
 
 const searchValue = JSON.parse(localStorage.getItem("searchValue") || "[]") as string[];
 
-function saveToLocalStorage(): void {
-    const treatedValue = RoutesEnum.findClosestTextMatch(inputValue.value) as string;
-    if (!searchValue.includes(treatedValue)) {
+function saveToLocalStorage(input: string): void {
+    const treatedValue = RoutesEnum.findClosestTextMatch(input) as string;
+
+    if (
+        !searchValue.includes(treatedValue) &&
+        treatedValue !== undefined &&
+        treatedValue !== "" &&
+        treatedValue !== null
+    ) {
         if (searchValue.length === 4) {
-            searchValue.shift();
+            searchValue.pop();
         }
-        searchValue.push(treatedValue);
+        searchValue.unshift(treatedValue);
     }
 
     localStorage.setItem("searchValue", JSON.stringify(searchValue));
@@ -77,12 +83,11 @@ onMounted(() => {
         >
             <span class="text-xs">⌘</span>J
         </kbd>
-        <CommandDialog v-model:open="isFocused" class="flex">
+        <CommandDialog v-model:open="isFocused" class="flex h-[350px]">
             <div class="flex items-center border-b bg-darkAlt pl-4" cmdk-input-wrapper>
                 <CommandInput
                     placeholder="Pesquisar..."
                     @input="inputValue = $event.target.value"
-                    @keyup.enter="saveToLocalStorage"
                     :icon-text="'light'"
                 />
             </div>
@@ -92,94 +97,146 @@ onMounted(() => {
                     v-if="searchValue.length > 0 && inputValue.length === 0"
                 >
                     <CommandItem
-                        @click="saveToLocalStorage"
+                        @click="saveToLocalStorage(item)"
                         v-for="item in searchValue"
                         :key="item"
                         :to="RoutesEnum.findClosestRouteMatch(item)"
                         :value="item"
                     >
-                        <CountdownTimerIcon class="mr-2 h-4 w-4 text-light" />
-                        <span class="text-light">{{ item }}</span>
+                        <span class="text-lg text-light material-symbols-outlined"> history </span>
+                        <span class="ml-2 text-light">{{ item }}</span>
                     </CommandItem>
                 </CommandGroup>
                 <CommandSeparator />
                 <CommandEmpty>Nenhum resultado encontrado.</CommandEmpty>
                 <CommandGroup heading="Sugestões" v-if="inputValue.length === 0">
-                    <CommandItem value="Criar" to="/reminders/create" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Criar"
+                        to="/reminders/create"
+                        @click="saveToLocalStorage('Criar')"
+                    >
                         <PlusIcon class="mr-2 h-4 w-4 text-light" />
                         <span class="text-light">Criar</span>
                     </CommandItem>
-                    <CommandItem value="Seus Lembretes" to="/reminders" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Seus Lembretes"
+                        to="/reminders"
+                        @click="saveToLocalStorage('Seus Lembretes')"
+                    >
                         <span class="material-icons mr-2 text-base text-light">table_view</span>
                         <span class="text-light">Seus Lembretes</span>
                     </CommandItem>
                 </CommandGroup>
                 <CommandGroup heading="Recomendado" v-if="inputValue.length > 0">
-                    <CommandItem value="Configurações" to="/settings" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Configurações"
+                        to="/settings"
+                        @click="saveToLocalStorage('Configurações')"
+                    >
                         <span class="material-icons mr-2 text-base text-light">settings</span>
                         <span class="text-light">Configurações</span>
                     </CommandItem>
-                    <CommandItem value="Página Inicial" to="/" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Página Inicial"
+                        to="/"
+                        @click="saveToLocalStorage('Página Inicial')"
+                    >
                         <span class="material-icons mr-2 text-base text-light">home</span>
                         <span class="text-light">Página Inicial</span>
                     </CommandItem>
-                    <CommandItem value="Seus Lembretes" to="/reminders" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Seus Lembretes"
+                        to="/reminders"
+                        @click="saveToLocalStorage('Seus Lembretes')"
+                    >
                         <span class="material-icons mr-2 text-base text-light">table_view</span>
                         <span class="text-light">Seus Lembretes</span>
                     </CommandItem>
-                    <CommandItem value="Gráficos" to="/graphs" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Gráficos"
+                        to="/graphs"
+                        @click="saveToLocalStorage('Gráficos')"
+                    >
                         <span class="material-icons mr-2 text-base text-light">view_kanban</span>
                         <span class="text-light">Gráficos</span>
                     </CommandItem>
-                    <CommandItem value="Criar" to="/reminders/create" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Criar"
+                        to="/reminders/create"
+                        @click="saveToLocalStorage('Criar')"
+                    >
                         <span class="material-icons mr-2 text-base text-light">add</span>
                         <span class="text-light">Criar</span>
                     </CommandItem>
-                    <CommandItem value="Raiva" to="/reminders/angry" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Raiva"
+                        to="/reminders/angry?page=1"
+                        @click="saveToLocalStorage('Raiva')"
+                    >
                         <i class="fi fi-rr-angry mt-1 mr-2 text-light"></i>
                         <span class="text-light">Raiva</span>
                     </CommandItem>
                     <CommandItem
                         value="Ansiedade"
-                        to="/reminders/anxiety"
-                        @click="saveToLocalStorage"
+                        to="/reminders/anxiety?page=1"
+                        @click="saveToLocalStorage('Ansiedade')"
                     >
                         <i class="fi fi-rr-face-explode mt-1 mr-2 text-light"></i>
                         <span class="text-light">Ansiedade</span>
                     </CommandItem>
-                    <CommandItem value="Tristeza" to="/reminders/sad" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Tristeza"
+                        to="/reminders/sad?page=1"
+                        @click="saveToLocalStorage('Tristeza')"
+                    >
                         <i class="fi fi-rr-sad-tear mt-1 mr-2 text-light"></i>
                         <span class="text-light">Tristeza</span>
                     </CommandItem>
-                    <CommandItem value="Inveja" to="/reminders/envy" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Inveja"
+                        to="/reminders/envy?page=1"
+                        @click="saveToLocalStorage('Inveja')"
+                    >
                         <i class="fi fi-rr-face-unamused mt-1 mr-2 text-light"></i>
                         <span class="text-light">Inveja</span>
                     </CommandItem>
                     <CommandItem
                         value="Nojinho"
-                        to="/reminders/disgust"
-                        @click="saveToLocalStorage"
+                        to="/reminders/disgust?page=1"
+                        @click="saveToLocalStorage('Nojinho')"
                     >
                         <i class="fi fi-rr-face-confounded mt-1 mr-2 text-light"></i>
                         <span class="text-light">Nojinho</span>
                     </CommandItem>
-                    <CommandItem value="Tédio" to="/reminders/boredom" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Tédio"
+                        to="/reminders/boredom?page=1"
+                        @click="saveToLocalStorage('Tédio')"
+                    >
                         <i class="fi fi-rr-meh mt-1 mr-2 text-light"></i>
                         <span class="text-light">Tédio</span>
                     </CommandItem>
                     <CommandItem
                         value="Alegria"
-                        to="/reminders/happiness"
-                        @click="saveToLocalStorage"
+                        to="/reminders/happiness?page=1"
+                        @click="saveToLocalStorage('Alegria')"
                     >
                         <i class="fi fi-rr-grin mt-1 mr-2 text-light"></i>
                         <span class="text-light">Alegria</span>
                     </CommandItem>
-                    <CommandItem value="Medo" to="/reminders/fear" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Medo"
+                        to="/reminders/fear?page=1"
+                        @click="saveToLocalStorage('Medo')"
+                    >
                         <i class="fi fi-rr-face-sad-sweat mt-1 mr-2 text-light"></i>
                         <span class="text-light">Medo</span>
                     </CommandItem>
-                    <CommandItem value="Vergonha" to="/reminders/shame" @click="saveToLocalStorage">
+                    <CommandItem
+                        value="Vergonha"
+                        to="/reminders/shame?page=1"
+                        @click="saveToLocalStorage('Vergonha')"
+                    >
                         <i class="fi fi-rr-tired mt-1 mr-2 text-light"></i>
                         <span class="text-light">Vergonha</span>
                     </CommandItem>
