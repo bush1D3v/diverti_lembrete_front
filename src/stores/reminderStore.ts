6d98b70f5f2c70095f7ba6f459e0c6fe175bfe7b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:10:45 -0300

    feat: Create reminder store

--- src/stores/reminderStore.ts
+++ src/stores/reminderStore.ts
@@ -6,20 +6,47 @@ import type { Reminder } from "@/types/Reminder";
 export const useReminderStore = defineStore("reminder", () => {
     const reminders = ref<Reminder[]>([]);
 
-    function addReminder(reminder: Reminder) {
+    function addReminder(reminder: Reminder): void {
         reminders.value.push(reminder);
     }
 
-    function removeReminder(id: string) {
+    function removeReminder(id: string): void {
         reminders.value = reminders.value.filter((reminder) => reminder.id !== id);
     }
 
-    function updateReminder(updatedReminder: Reminder) {
-        const index = reminders.value.findIndex((reminder) => reminder.id === updatedReminder.id);
-        if (index !== -1) {
-            reminders.value[index] = updatedReminder;
+    function checkReminder(id: string, check: boolean): void {
+        const reminder = reminders.value.find((reminder) => reminder.id === id);
+        if (!reminder) {
+            return;
         }
+        reminder.check = check;
     }
 
-    return { reminders, addReminder, removeReminder, updateReminder };
+    function detailReminder(id: string): Reminder | undefined {
+        const reminder = reminders.value.find((reminder) => reminder.id === id);
+        if (!reminder) {
+            return;
+        }
+        return reminder;
+    }
+
+    function getChecksValue(): number {
+        return reminders.value.filter((reminder) => reminder.check).length;
+    }
+
+    function getUnchecksValue(): number {
+        return reminders.value.filter((reminder) => !reminder.check).length;
+    }
+
+    return { reminders, addReminder, removeReminder, checkReminder, detailReminder, getChecksValue, getUnchecksValue };
+});
+
+export const useRecentRemindersStore = defineStore("recentReminders", () => {
+    const reminders = ref<Reminder[]>([]);
+
+    function addRecentReminder(reminder: Reminder): void {
+        reminders.value.push(reminder);
+    }
+
+    return { reminders, addRecentReminder };
 });
