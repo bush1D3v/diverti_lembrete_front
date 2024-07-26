685b4fa3cc244d2f235a7a8a57ece4124daf8f1f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-26 09:13:34 -0300

    update: Update not found view page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+import { EmotionsEnum } from "@/enum/EmotionsEnum";
 import NotFoundImage from "@/assets/404-image.svg";
 import Button from "@/components/ui/button/Button.vue";
+
+const route = useRoute();
+const emotion = route.params.emotion;
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center gap-10 min-h-[95dvh] w-full">
+    <div
+        v-if="emotion"
+        class="flex flex-col items-center justify-center gap-10 min-h-[95dvh] w-full"
+    >
+        <div class="flex flex-col items-center text-center gap-4">
+            <h1 class="text-4xl font-bold text-light max-w-2xl">
+                Você tentou acessar a emoção: "{{ EmotionsEnum.translateEmotionToPT(emotion) }}",
+                que ainda não existe
+            </h1>
+            <p class="text-base font-semibold text-accent max-w-3xl">
+                A emoção que você tentou acessar não existe, mas você pode criá-la. Clique no botão
+                abaixo para criar um novo lembrete emocionante.
+            </p>
+        </div>
+        <img :src="NotFoundImage" alt="Not Found Image" />
+        <div class="flex gap-20">
+            <router-link to="/reminders/create" class="text-primary underline mt-2">
+                <Button
+                    class="mt-6 rounded-full text-lg font-semibold p-4 hover:opacity-50 duration-150 ease-in-out"
+                >
+                    Criar Lembrete
+                </Button>
+            </router-link>
+        </div>
+    </div>
+    <div v-else class="flex flex-col items-center justify-center gap-10 min-h-[95dvh] w-full">
         <div class="flex flex-col items-center text-center gap-4">
             <h1 class="text-4xl font-bold text-light max-w-xl">
                 Você tentou acessar uma página <br />
@@ -19,14 +49,14 @@ import Button from "@/components/ui/button/Button.vue";
         <div class="flex gap-20">
             <router-link to="/" class="text-primary underline mt-2">
                 <Button
-                    class="mt-6 rounded-full text-lg font-semibold p-4 hover:bg-primaryAlt duration-150 ease-in-out"
+                    class="mt-6 rounded-full text-lg font-semibold p-4 hover:opacity-50 duration-150 ease-in-out"
                 >
                     Página Inicial
                 </Button>
             </router-link>
             <router-link to="/reminders" class="text-primary underline mt-2">
                 <Button
-                    class="mt-6 rounded-full text-lg font-semibold p-4 hover:bg-primaryAlt duration-150 ease-in-out"
+                    class="mt-6 rounded-full text-lg font-semibold p-4 hover:opacity-50 duration-150 ease-in-out"
                 >
                     Seus Lembretes
                 </Button>
